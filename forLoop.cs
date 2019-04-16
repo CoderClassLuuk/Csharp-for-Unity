1fc42ef42b29f076f4da3c9faa6c8052818668af
Luuk <31882018+CoderClassLuuk@users.noreply.github.com>
2019-04-16 14:40:04 +0200

    For loop opdracht gemaakt

---
+++ forLoop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string geheimeWoord = "FIETS";
+            for (int i = 1; i < 4; i++)
+            {
+                Console.WriteLine("Raad het geheime woord\nTIP : HET RIJMT OP IETS\n: ");
+                string woordInput = Console.ReadLine();
+                if (woordInput.ToUpper() == geheimeWoord) 
+                {
+                    Console.WriteLine("Het is je gelukt! Ik ben trots op je :).");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Ok buddy\nretard.\nAantal pogingen over: " + Convert.ToString((3 - i)));
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
