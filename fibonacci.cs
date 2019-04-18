d969f1b9a3242a127d720a8e9b06d15b5daa814e
Luuk <31882018+CoderClassLuuk@users.noreply.github.com>
2019-04-18 09:05:57 +0200

    Fibonacci reeks opdracht gemaakt

---
+++ fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    class Program
+    {
+        public static int fibonacci(int n)
+        {
+            int fibonacciFirst = 0;
+            int fibonacciSecond = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int fibonacciTemporary = fibonacciFirst;
+                fibonacciFirst = fibonacciSecond;
+                fibonacciSecond = fibonacciTemporary + fibonacciSecond;
+            }
+            return fibonacciFirst;
+        }
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(fibonacci(i) + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
